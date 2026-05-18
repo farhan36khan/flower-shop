cb105d03ecf0f01ea5e08fbf47d76ef7432d3539
farhan36khan <147793354+farhan36khan@users.noreply.github.com>
2026-05-18 12:57:42 +0500

    Fix Context import

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);*/
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./Context/CartContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
     <App />
